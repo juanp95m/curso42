a9200ae046ee90b5807b41fb401d7393ae68ed39
Juan Perez Morente <jperez-m@c2r2s1.42malaga.com>
2025-05-05 20:48:26 +0200

    subida6

--- libft/libft.h
+++ libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: jperez-m <jperez-m@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/24 15:41:01 by jperez-m          #+#    #+#             */
-/*   Updated: 2025/05/02 09:35:17 by jperez-m         ###   ########.fr       */
+/*   Updated: 2025/05/05 18:52:54 by jperez-m         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,4 +34,6 @@ void	*ft_memmove(void *dst, const void *src, size_t n);
 int     ft_strncmp(const char *s1, const char *s2, size_t n);
 char	*ft_strchr(const char *s, int c);
 char	*ft_strrchr(const char*s, int c);
+size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize);
+size_t	ft_strlcat(char *dst, const char *src, size_t dstsize);
 #endif
