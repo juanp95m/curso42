236769137af24131e6c919fbbe0c9923446afd5c
Juan Perez Morente <jperez-m@c2r7s3.42malaga.com>
2025-05-07 21:24:53 +0200

    subida8

--- libft/ft_strnstr.c
+++ libft/ft_strnstr.c
@@ -6,7 +6,7 @@
 /*   By: jperez-m <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/06 20:52:45 by jperez-m          #+#    #+#             */
-/*   Updated: 2025/05/06 21:35:44 by jperez-m         ###   ########.fr       */
+/*   Updated: 2025/05/07 16:15:47 by jperez-m         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,11 +23,11 @@ char	*ft_strnstr(const char *big, const char *little, size_t len)
 	while (big[i] != '\0' && i < len)
 	{
 		j = 0;
-		while (big[i + j] == little[j] && (i + j) < len && little[j] != '\0')
+		while (big[i + j] == little[j] && (i + j) < len)
 		{
-			if (little[j+i] == '\0')
-				return ((char *) &big[i]);
 			j++;
+			if (little[j] == '\0')
+				return ((char *) &big[i]);
 		}
 		i++;
 	}
