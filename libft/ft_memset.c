88626c737d0dac2a7c073052cf089fdd318ab238
Juan Perez Morente <jperez-m@c2r6s3.42malaga.com>
2025-05-14 20:19:57 +0200

    subida11

--- libft/ft_memset.c
+++ libft/ft_memset.c
@@ -3,13 +3,13 @@
 /*                                                        :::      ::::::::   */
 /*   ft_memset.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jperez-m <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: jperez-m <jperez-m@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/22 20:28:54 by jperez-m          #+#    #+#             */
-/*   Updated: 2025/04/30 18:39:37 by jperez-m         ###   ########.fr       */
+/*   Updated: 2025/05/14 20:16:17 by jperez-m         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-#include "libft.h"
+
 #include <stdio.h>
 
 void	*ft_memset(void *b, int c, size_t n)
@@ -17,13 +17,12 @@ void	*ft_memset(void *b, int c, size_t n)
 	size_t			i;
 	unsigned char	*temp;
 
-	temp = (unsigned char *) b;
+	temp = b;
 	i = 0;
 	while (i < n)
 	{
-		temp [i] = c;
+		temp[i] = c;
 		i++;
 	}
-	b = (void *)temp;
 	return (b);
 }
