45072f7de1fe74b98e1989c9a8e96da779ac69f1
Juan Perez Morente <jperez-m@c2r3s3.42malaga.com>
2025-10-16 12:25:03 +0200

    tercerfractol

---
+++ fractol/init.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   init.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jperez-m <jperez-m@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/10/16 11:00:41 by jperez-m          #+#    #+#             */
+/*   Updated: 2025/10/16 11:00:42 by jperez-m         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "fractol.h"
+
+void	init_fractal(t_fractal *fractal)
+{
+	fractal->mlx = mlx_init(WIDTH, HEIGHT, "fract-ol", true);
+	if (!fractal->mlx)
+		exit(EXIT_FAILURE);
+	fractal->img = mlx_new_image(fractal->mlx, WIDTH, HEIGHT);
+	if (!fractal->img)
+	{
+		mlx_terminate(fractal->mlx);
+		exit(EXIT_FAILURE);
+	}
+	fractal->min_r = -2.0;
+	fractal->max_r = 2.0;
+	fractal->min_i = -2.0;
+	fractal->max_i = 2.0;
+}
---
+++ fractol/utils.c
@@ -0,0 +1,18 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   utils.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jperez-m <jperez-m@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/10/16 11:01:12 by jperez-m          #+#    #+#             */
+/*   Updated: 2025/10/16 11:01:13 by jperez-m         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "fractol.h"
+
+double	map(double unscaled_num, double new_min, double new_max, double old_max)
+{
+	return ((new_max - new_min) * (unscaled_num / old_max) + new_min);
+}
